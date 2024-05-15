756615bdc10ac19f30ff6ab09c24f4a6e2f91a49
Mohamad Shahin <mohamad.shahin2002@gmail.com>
2024-05-15 22:01:57 +0300

    latest

--- infer_image.py
+++ infer_image.py
@@ -10,14 +10,13 @@ with open('roboflow_config.json') as f:
     FRAMERATE = config["FRAMERATE"]
     BUFFER = config["BUFFER"]
 
-from io import BytesIO
 import asyncio
 import cv2
 import base64
-import numpy as np
 import httpx
+import supervision as sv
 import time
-from utils import read_license_plate, crop_image, detect_text
+from utils import clear_output, crop_image_using_box, read_license_plate, crop_image, detect_text, upscale_result
 
 # Construct the Roboflow Infer URL
 # (if running locally replace https://detect.roboflow.com/ with eg http://127.0.0.1:9001/)
@@ -41,8 +40,16 @@ upload_url_json = "".join([
     "&confidence=55"
 ])
 
+tracker = sv.ByteTrack(track_activation_threshold=0.55, lost_track_buffer=24, minimum_matching_threshold=0.9, frame_rate=24)
+box_annotator = sv.BoundingBoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
 # Get webcam interface via opencv-python
-video = cv2.VideoCapture(1)
+video = cv2.VideoCapture("tcp://raspberrypi.local:5556")
+
+json_sink = sv.JSONSink("./cropped/output.json")
+
+detected_detections = set()
 
 # Infer via the Roboflow Infer API and return the result
 # Takes an httpx.AsyncClient as a parameter
@@ -60,36 +67,57 @@ async def infer(requests):
     img_str = base64.b64encode(buffer)
 
     # Get prediction from Roboflow Infer API
-    resp = await requests.post(upload_url, data=img_str, headers={
-        "Content-Type": "application/x-www-form-urlencoded"
-    })
+    # resp = await requests.post(upload_url, data=img_str, headers={
+    #     "Content-Type": "application/x-www-form-urlencoded"
+    # })
     resp_json = await requests.post(upload_url_json, data=img_str, headers={
         "Content-Type": "application/x-www-form-urlencoded"
     })
 
     resp_json = json.loads((resp_json.content).decode('utf-8'))
+    upscaled_result = upscale_result(resp_json, max(height, width) / ROBOFLOW_SIZE)
+    detections = sv.Detections.from_inference(upscaled_result)
+    detections = tracker.update_with_detections(detections)
+    
+    labels = [
+        f"#{tracker_id}"
+        for tracker_id
+        in detections.tracker_id
+    ]
+    for i in range(abs(len(labels) - len(detections))):
+        labels.append(f"${i+1}")
+
+    # print(f"trackers: {len(detections.tracker_id)}, labels: {len(labels)}")
+
     predictions = resp_json["predictions"]
     # image_height = resp_json["image"]["height"]
     # image_width = resp_json["image"]["width"]
 
-    image = np.asarray(bytearray(resp.content), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # image = np.asarray(bytearray(resp.content), dtype="uint8")
+    # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
     reverse_scale = max(height, width) / ROBOFLOW_SIZE
 
-    for prediction in predictions:
-        x_original = round(prediction["x"] * reverse_scale)
-        y_original = round(prediction["y"] * reverse_scale)
-        width_original = round(prediction["width"] * reverse_scale)
-        height_original = round(prediction["height"] * reverse_scale)
-        crop_image(img, x_original, y_original, width_original, height_original, height, width)
-        detect_text('./cropped/cropped.jpg')
-        # await asyncio.sleep(1)
+    for xyxy, id in zip(detections.xyxy, detections.tracker_id):
+        if (id in detected_detections):
+            continue
+        # x_original = round(prediction["x"] * reverse_scale)
+        # y_original = round(prediction["y"] * reverse_scale)
+        # width_original = round(prediction["width"] * reverse_scale)
+        # height_original = round(prediction["height"] * reverse_scale)
+        # crop_image(img, x_original, y_original, width_original, height_original, height, width)
+        crop_image_using_box(img, xyxy[0], xyxy[1], xyxy[2], xyxy[3], id)
+        with json_sink as sink:
+            sink.append(detections, {"plate-number": detect_text(f'./cropped/{id}.jpg')})
+        detected_detections.add(id)
 
-    return image
+    boxed_frame = box_annotator.annotate(img.copy(), detections=detections)
+    return label_annotator.annotate(boxed_frame, detections=detections, labels=labels)
 
 # Main loop; infers at FRAMERATE frames per second until you press "q"
 async def main():
+    clear_output()
+
     # Initialize
     last_frame = time.time()
 
@@ -104,8 +132,8 @@ async def main():
 
             # Throttle to FRAMERATE fps and print actual frames per second achieved
             elapsed = time.time() - last_frame
-            await asyncio.sleep(max(0, 1/FRAMERATE - elapsed))
-            await asyncio.sleep(1)
+            # await asyncio.sleep(max(0, 1/FRAMERATE - elapsed))
+            # await asyncio.sleep(1)
             print((1/(time.time()-last_frame)), " fps")
             last_frame = time.time()
 
@@ -119,9 +147,10 @@ async def main():
 
             # Remove the first image from our buffer
             # wait for it to finish loading (if necessary)
-            image = await futures.pop(0)
-            # And display the inference results
-            cv2.imshow('image', image)
+            if len(futures) > 0:
+                image = await futures.pop(0)
+                # And display the inference results
+                cv2.imshow('image', image)
 
 # Run our main loop
 asyncio.run(main())
--- utils.py
+++ utils.py
@@ -1,11 +1,42 @@
 import easyocr
 import numpy as np
+import copy
 import cv2
+import asyncio
 from PIL import Image
 
 
 # Initialize the OCR reader
 reader = easyocr.Reader(['en'], gpu=False)
+
+import os, shutil
+folder = './cropped'
+def clear_output():
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def crop_image_using_box(img: np.ndarray, x1, y1, x2, y2, id):
+    x1 = int(x1)
+    y1 = int(y1)
+    x2 = int(x2)
+    y2 = int(y2)
+    cropped_array = img[y1:y2, x1:x2]
+
+    license_plate_crop_gray = cv2.cvtColor(cropped_array, cv2.COLOR_BGR2GRAY)
+    # _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_OTSU)
+    # cv2.imshow("Cropped", license_plate_crop_gray)
+    # if cv2.waitKey(1) == ord('q'):
+    #     cv2.destroyAllWindows()
+    cropped_image = Image.fromarray(license_plate_crop_gray)
+    cropped_image.save(f"./cropped/{id}.jpg")
+
 
 def crop_image(img: np.ndarray, x, y, width, height, img_height, img_width):
     x = int(x)
@@ -29,18 +60,22 @@ def crop_image(img: np.ndarray, x, y, width, height, img_height, img_width):
     end_x = min(end_x, img_width)
     end_y = min(end_y, img_height)
 
+    # print(f"start_x:{start_x}, end_x:{end_x} start_y:{start_y}, end_y:{end_y}, height:{height} width:{width} img_height:{img_height} img_width:{img_width}")
 
     # Crop the image using NumPy slicing
     cropped_array = img[start_y:end_y, start_x:end_x]
 
+
     # Convert the cropped array back to an image to save or display
     license_plate_crop_gray = cv2.cvtColor(cropped_array, cv2.COLOR_BGR2GRAY)
     # _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_OTSU)
-
+    # cv2.imshow("Cropped", license_plate_crop_gray)
+    # if cv2.waitKey(1) == ord('q'):
+    #     cv2.destroyAllWindows()
     cropped_image = Image.fromarray(license_plate_crop_gray)
     cropped_image.save("./cropped/cropped.jpg")
 
-    return license_plate_crop_gray
+    return (start_x, start_y, end_x, end_y)
 
 def read_license_plate(license_plate_crop):
     """
@@ -64,7 +99,7 @@ def read_license_plate(license_plate_crop):
 
     return None, None
 
-def detect_text(path):
+async def detect_text(path):
     """Detects text in the file."""
     from google.cloud import vision
 
@@ -77,7 +112,6 @@ def detect_text(path):
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print("Texts:")
     texts_list = []
 
     for text in texts:
@@ -89,13 +123,13 @@ def detect_text(path):
         # ]
 
         # print("bounds: {}".format(",".join(vertices)))
-    print(format_number_plate(texts_list))
-
+    print(texts_list)
     if response.error.message:
         raise Exception(
             "{}\nFor more info on error messages, check: "
             "https://cloud.google.com/apis/design/errors".format(response.error.message)
         )
+    return format_number_plate(texts_list)[0]
 
 def format_number_plate(plate_numbers: list[str]):
     '''
@@ -108,7 +142,8 @@ def format_number_plate(plate_numbers: list[str]):
         plate_numbers: List of potential number plates
     '''
     # remove first element
-    plate_numbers.pop(0)
+    if len(plate_numbers) > 0:
+        plate_numbers.pop(0)
 
     # supported countries
     supported_countries = {
@@ -226,3 +261,17 @@ def unknown_format(strings: list[str]):
     result.append("".join(strings))
 
     return result
+
+def upscale_result(result: dict, scale):
+    upscaled_result = copy.deepcopy(result)
+
+    upscaled_result["image"]["height"] = round(upscaled_result["image"]["height"] * scale)
+    upscaled_result["image"]["width"] = round(upscaled_result["image"]["width"] * scale)
+
+    for prediction in upscaled_result["predictions"]:
+        prediction["x"] = round(prediction["x"] * scale)
+        prediction["y"] = round(prediction["y"] * scale)
+        prediction["width"] = round(prediction["width"] * scale)
+        prediction["height"] = round(prediction["height"] * scale)
+    # print(result, upscaled_result)
+    return upscaled_result
